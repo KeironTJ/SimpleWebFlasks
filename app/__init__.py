ffd0916138ad3a2d3fd6a775a5cf6cfd39cc8c75
KeironTJ <135234970+KeironTJ@users.noreply.github.com>
2024-07-24 13:41:15 +0000

    Further development of websockets

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
-    login.init_app(app)
+    login.init_app(app, cors_allowed_origins="*")
 
     socketio.init_app(app)
     from app.events import game_events
