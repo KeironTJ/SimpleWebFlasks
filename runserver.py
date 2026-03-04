05dd1891e4a1b9c2b443f2a251ac577623bdcdd4
keirontj <keirontj@gmail.com>
2026-03-04 22:14:06 +0000

    .

--- runserver.py
+++ runserver.py
@@ -7,7 +7,7 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(app, debug=True)
+    app.run(debug=True)
 
 @app.shell_context_processor
 def make_shell_context():
